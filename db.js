8bd9b5cfb3dcd81382c8067d46f8eec36d96e8cd
rkurenok <48415490+rkurenok@users.noreply.github.com>
2023-04-11 09:59:37 +0300

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("authorization", "postgres", "root", {
-    dialect: "postgres",
-    host: "localhost",
-});
-
-module.exports = sequelize;
